5302f08fd33e28f60f5ac8c25f764e8f6ef4801f
German <german.mumma@asaquality.ee>
2017-11-09 18:15:27 +0200

    Add modal to specification page

--- src/app/specification/specification.module.ts
+++ src/app/specification/specification.module.ts
@@ -3,12 +3,15 @@ import {RouterModule} from "@angular/router";
 import {SPECIFICATION_ROUTE} from "./specification.route";
 import {SpecificationComponent} from "./specification.component";
 import {CommonModule} from "@angular/common";
+import {AddSpecificationModalComponent} from "./modal/add.component";
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forRoot([SPECIFICATION_ROUTE], {useHash: true})],
-  declarations: [SpecificationComponent]
+  declarations: [SpecificationComponent,
+    AddSpecificationModalComponent],
+  bootstrap: []
 })
 export class SpecificationModule {
 
