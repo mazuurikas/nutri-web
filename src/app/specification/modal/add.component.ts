7695c9bdbf514eb1a11196292ddf9ff9667bab4d
German <german.mumma@asaquality.ee>
2017-11-09 19:51:55 +0200

    Save specification modal to database

--- src/app/specification/modal/add.component.ts
+++ src/app/specification/modal/add.component.ts
@@ -1,24 +1,33 @@
 import {Component} from '@angular/core';
 
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import {Specification} from "../specification.model";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-add-specification',
   templateUrl: './add.template.html'
 })
 export class AddSpecificationModalComponent {
-  specification = new Specification(0, "", "", "", 2, 3, 5, 4, 3, 2, 2, 2);
+  ngbModalRef: NgbModalRef;
+  specification = new Specification("", "", "", 2, 3, 5, 4, 3, 2, 2, 2);
 
-  constructor(private modalService: NgbModal) {
+  constructor(private modalService: NgbModal,
+              private http: HttpClient) {
   }
 
   open(content) {
-    this.modalService.open(content);
+    this.ngbModalRef = this.modalService.open(content);
   }
 
   save() {
-    console.log(this.specification);
+    this.http.post('http://localhost:8181/specifications', this.specification).subscribe(
+      () => {
+        this.ngbModalRef.close();
+      },
+      (data) => {
+        console.log(data);
+      }
+    )
   }
-
 }
--- src/app/specification/specification.model.ts
+++ src/app/specification/specification.model.ts
@@ -1,6 +1,5 @@
 export class Specification {
-  constructor(public id: number,
-              public name: string,
+  constructor(public name: string,
               public producer: string,
               public comment: string,
               public energyKj: number,
